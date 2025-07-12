6b7824e57fc5cf2b1c49695cc68474dca9d34ef7
George Jeff Onyango <82260959+gojeffonyango@users.noreply.github.com>
2025-07-12 21:00:43 +0300

    Add files via upload

---
+++ polymorphism_with_funcs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 12 20:59:27 2025
+
+@author: gojef
+"""
+
+def add(a, b):
+    return a + b
+
+# Works with integers
+print(add(5, 10))  # Output: 15
+
+# Works with strings
+print(add("Hello, ", "World!"))  # Output: Hello, World!
+
+# Works with lists
+print(add([1, 2], [3, 4]))  # Output: [1, 2, 3, 4]
